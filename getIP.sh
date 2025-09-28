91e56a9ab1320529761b6e018fd23f3636cc8c58
Daniel Stellmon <Dstellm@gmail.com>
2025-09-28 09:48:08 -0500

    added ToDos

--- getIP.sh
+++ getIP.sh
@@ -1,6 +1,8 @@
 # Host address format http://{example.com}:####
+#TODO: replace this with read yaml
 Host=http://barqu.xyz:8000
 
+#TODO: Do I really still need this? It should throw an error is the setting file is missing.
 # If there is no settings.txt file create one
 if [ ! -f "settings.txt" ]; then
     touch settings.txt
@@ -10,7 +12,7 @@ fi
 # Current WAN IP address
 CurrentIP=$(curl $Host | grep "RemoteAddr:" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 # Look up previous WAN IP address
-PreviousIP=$(cat settings.txt)
+PreviousIP=$(cat settings.txt) #TODO: This needs to change as well
 
 #compare the two addresses. If they are NOT the same
 if [ $CurrentIP != $PreviousIP ]; then
